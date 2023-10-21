7d0870b9502df1b265e1c366c9f3b7f76c297b6e
jaffarzyed <85126655+jaffarzyed@users.noreply.github.com>
2023-10-21 12:31:33 +0300

    Update image_builder_govc.sh
    Update the IMAGEBUILDER_TARBALL_URI to v0.2.1

--- eksa-admin-machine/terraform/scripts/image_builder_govc.sh
+++ eksa-admin-machine/terraform/scripts/image_builder_govc.sh
@@ -10,7 +10,8 @@ sudo apt install python3 python3-setuptools python3-dev python3-pip -y
 echo "HostKeyAlgorithms +ssh-rsa" >> /home/$USER/.ssh/config
 echo "PubkeyAcceptedKeyTypes +ssh-rsa" >> /home/$USER/.ssh/config
 cd /tmp
-sudo wget https://anywhere-assets.eks.amazonaws.com/releases/bundles/15/artifacts/image-builder/0.1.0/image-builder-linux-amd64.tar.gz
+#sudo wget https://anywhere-assets.eks.amazonaws.com/releases/bundles/15/artifacts/image-builder/0.1.0/image-builder-linux-amd64.tar.gz
+sudo wget https://anywhere-assets.eks.amazonaws.com/releases/bundles/49/artifacts/image-builder/v0.2.1/image-builder-linux-amd64.tar.gz
 sudo tar -xzf image-builder*.tar.gz
 sudo cp image-builder /usr/local/bin
 sudo curl -L -o - https://github.com/vmware/govmomi/releases/latest/download/govc_$(uname -s)_$(uname -m).tar.gz | sudo tar -C /usr/local/bin -xvzf - govc
